60e4882d66689eba1435ccd65fdae701b9415a2a
tylersammann@gmail.com <tylersammann@gmail.com@a32e8fe7-e422-d660-1f10-772f3128b035>
2011-11-03 05:49:37 +0000

    added package and starting code to load and play mp3 files

---
+++ trunk/PC/src/fermataSOUNDOUT/LoadSoundFile.java
@@ -0,0 +1,40 @@
+package fermataSOUNDOUT;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+ 
+import javazoom.jl.player.Player;
+ 
+public class LoadSoundFile {
+    private String filename;
+    private Player player;
+ 
+    // constructor that takes the name of an MP3 file
+    public LoadSoundFile(String filename) {
+        this.filename = filename;
+    }
+ 
+    // play the MP3 file
+    public void play() {
+        try {
+            FileInputStream fis     = new FileInputStream(filename);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            player = new Player(bis);
+            player.play();
+        }
+        catch (Exception e) {
+            System.out.println("Problem playing file " + filename);
+            System.out.println(e);
+        }
+ 
+    }
+ 
+/*    public static void main(String[] args) {
+ 
+        //plays 07.mp3 file located at C drive
+    	LoadSoundFile mp3 = new LoadSoundFile("c:/07.mp3");
+        mp3.play();
+ 
+    }*/
+ 
+}
