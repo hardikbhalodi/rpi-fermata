29b3ae102bad1c46b01e9ec85b0b42247f083021
tylersammann@gmail.com <tylersammann@gmail.com@a32e8fe7-e422-d660-1f10-772f3128b035>
2011-12-01 02:51:14 +0000

    new filters

---
+++ trunk/PC/src/fermataPC/filters/DelayFilter.java
@@ -0,0 +1,47 @@
+/*package fermataPC.filters;
+ * 
+ */
+//TODO get delay filters working in the future
+/*
+import com.jfhpsyn.unitgen.InterpolatingDelay;
+
+import fermataPC.util.Coordinate;
+
+public class DelayFilter extends Filter
+{
+	private InterpolatingDelay delFilter;
+	
+	public DelayFilter(int axis)
+	{	
+		super.axis = axis;
+		name = "Delay ("+ (axis == 0 ? "x" : "y") + ")";
+		delFilter = new InterpolatingDelay();
+		
+		FilterProcessor.synth.add(delFilter);
+		delFilter.allocate(10000);
+		
+		input = delFilter.input;
+		output = delFilter.output;
+		
+	}
+	@Override
+	public void setCoordinate(Coordinate coord)
+	{
+		int val = coord.getValue();
+		
+		
+	}
+
+	@Override
+	public void cancelFilter() 
+	{
+		// TODO Auto-generated method stub
+		FilterProcessor.synth.remove(delFilter);
+		
+		//delFilter.stop();
+		delFilter.input.disconnectAll(0);
+		delFilter.output.disconnectAll(0);	
+		
+	}	
+	
+}*/
